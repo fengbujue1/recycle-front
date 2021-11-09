92e69a27f6357d15552842c10524f258bc3c6244
619577235@qq.com <Zhouyunji150431>
2021-11-09 22:45:21 +0800

    2021.11.9 订单页面编写 1.自定义组件bars的定义和使用 2.订单数据的请求（js中方法的定义）
    存在问题：
    异步请求数据的延迟加载问题

--- pages/order/order.js
+++ pages/order/order.js
@@ -5,14 +5,48 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        tabs: [
+            {
+              id: 0,
+              value: "未接单",
+              isActive: true
+            },
+            {
+              id: 1,
+              value: "已接单",
+              isActive: false
+            },
+            {
+              id: 2,
+              value: "已完成",
+              isActive: false
+            },
+            {
+              id: 3,
+              value: "已取消",
+              isActive: false
+            }
+          ],
+          ordersByType:[]
     },
 
+    handleTabsItemChange(e) {
+        // 1 获取被点击的标题索引
+        const index  = e.detail.index;
+        this.getOrdersByStatus(index+1)
+        // 2 修改源数组
+        let  tabs  = this.data.tabs;
+        tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
+        // 3 赋值到data中
+        this.setData({
+          tabs:tabs
+        })
+      },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+       
     },
 
     /**
@@ -26,14 +60,38 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-
+        this.getOrdersByStatus(1)
+        
     },
-
+ // 指定状态订单数据
+ getOrdersByStatus:function(statusPam){
+    var ordersByTyp= [];
+    wx.request({
+        method:"GET",
+        dataType:"json",
+        url: 'http://localhost:8080/recycle/recycle/query?userId=1&status=1',
+        data:{"userId":1,"status":statusPam},
+        success: (res) => {
+          //接收订单数据
+          const orders=res.data;
+          for (var i=0;i<orders.length;i++){ 
+            ordersByTyp[i]=orders[i];
+          };
+          this.setData({
+            ordersByType:ordersByTyp
+          });
+          console.log(this.data.ordersByType)
+        },
+        fail (res) {
+          console.log(res.data)
+        }
+      })   
+  },
     /**
      * 生命周期函数--监听页面隐藏
      */
     onHide: function () {
-
+    
     },
 
     /**
